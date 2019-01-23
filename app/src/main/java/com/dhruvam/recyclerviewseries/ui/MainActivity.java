233c67698f3787114e6f37e0d4c735251061203b
Dhruvam <dhruvamssharma@gmail.com>
2019-01-23 19:31:50 +0530

    design added

--- app/src/main/java/com/dhruvam/recyclerviewseries/ui/MainActivity.java
+++ app/src/main/java/com/dhruvam/recyclerviewseries/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package com.dhruvam.recyclerviewseries.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
