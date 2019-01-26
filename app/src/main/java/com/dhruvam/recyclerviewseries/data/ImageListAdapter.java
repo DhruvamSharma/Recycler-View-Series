43d39182e6eabd77707cef9c3fb3adc1c46e6328
Dhruvam <dhruvamssharma@gmail.com>
2019-01-26 16:00:06 +0530

    done with designing

--- app/src/main/java/com/dhruvam/recyclerviewseries/data/ImageListAdapter.java
+++ app/src/main/java/com/dhruvam/recyclerviewseries/data/ImageListAdapter.java
@@ -1,11 +1,14 @@
 package com.dhruvam.recyclerviewseries.data;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dhruvam.recyclerviewseries.R;
+import com.dhruvam.recyclerviewseries.ui.MainActivity;
 
 import java.util.ArrayList;
 
@@ -15,11 +18,13 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ImageListViewHolder> {
 
     ArrayList<String> data = new ArrayList<>();
+    private Context context;
 
-    public ImageListAdapter() {
+    public ImageListAdapter(Context context) {
         for(int i = 0; i< 2 ;i ++) {
             data.add("hello");
         }
+        this.context = context.getApplicationContext();
     }
 
     @NonNull
@@ -31,6 +36,15 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.Imag
 
     @Override
     public void onBindViewHolder(@NonNull ImageListViewHolder holder, int position) {
+        if(position%2 == 0) {
+            holder.reward.setImageResource(R.drawable.free_drink_mug);
+            holder.listItemText.setText(context.getResources().getString(R.string.free_drink_text));
+            holder.listItemCaption.setText(context.getResources().getString(R.string.free_drink_caption_text));
+        } else {
+            holder.reward.setImageResource(R.drawable.size_upgrade_mug);
+            holder.listItemText.setText(context.getResources().getString(R.string.size_upgrade_text));
+            holder.listItemCaption.setText(context.getResources().getString(R.string.size_upgrade_caption_text));
+        }
 
     }
 
@@ -40,11 +54,13 @@ public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.Imag
     }
 
     public class ImageListViewHolder extends RecyclerView.ViewHolder{
-        TextView listItemText;
+        TextView listItemText, listItemCaption;
+        ImageView reward;
         public ImageListViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            reward = itemView.findViewById(R.id.list_item_cup_iv);
             listItemText = itemView.findViewById(R.id.list_item_reward_headline_tv);
+            listItemCaption = itemView.findViewById(R.id.list_item_reward_caption_tv);
         }
     }
 }
--- app/src/main/java/com/dhruvam/recyclerviewseries/ui/MainActivity.java
+++ app/src/main/java/com/dhruvam/recyclerviewseries/ui/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.dhruvam.recyclerviewseries.R;
 import com.dhruvam.recyclerviewseries.data.ImageListAdapter;
@@ -14,17 +21,39 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mMainList;
     ImageListAdapter mListAdapter;
 
+    //Variable to store brightness value
+    private int brightness;
+    //Content resolver used as a handle to the system's settings
+    private ContentResolver cResolver;
+    //Window object, that will store a reference to the current window
+    private Window window;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if(Settings.System.canWrite(this)) {
+//                Settings.System.putInt(getContentResolver(),
+//                        Settings.System.SCREEN_BRIGHTNESS,
+//                        255);
+//            } else {
+//                startActivity( new Intent(this, android.settings.action.MANAGE_WRITE_SETTINGS));
+//            }
+//
+//        } else {
+//            Settings.System.putInt(getContentResolver(),
+//                    Settings.System.SCREEN_BRIGHTNESS,
+//                    255);
+//        }
+
         recyclerviewSetup();
     }
 
-
     private void recyclerviewSetup() {
         mMainList = findViewById(R.id.main_list);
-        mListAdapter = new ImageListAdapter();
+        mListAdapter = new ImageListAdapter(this);
         mMainList.setAdapter(mListAdapter);
         LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setOrientation(RecyclerView.VERTICAL);
